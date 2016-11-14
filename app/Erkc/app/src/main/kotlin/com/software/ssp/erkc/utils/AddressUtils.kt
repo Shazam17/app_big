4b201a07d979f4b35bf59191a5a5bf812cf14063
Orteney D.A <orteneyda@sib-soft.ru>
2016-11-14 12:06:19 +0700

    address parse hotfix

--- app/Erkc/app/src/main/kotlin/com/software/ssp/erkc/utils/AddressUtils.kt
+++ app/Erkc/app/src/main/kotlin/com/software/ssp/erkc/utils/AddressUtils.kt
@@ -5,12 +5,12 @@ fun splitFullAddress(address: String): List<String>{
     val mutableList = mutableListOf<String>()
 
     val HOUSE_DELIMITER = " д."
-    val APARTMENT_DELIMITER = " кв."
+    val APARTMENT_DELIMITER = "кв."
 
     val addressSplits = address.split(HOUSE_DELIMITER)
     mutableList.add(addressSplits[0])
     if(addressSplits.size>1) {
-        val houseAndApartmentSplits = addressSplits[1].split(APARTMENT_DELIMITER)
+        val houseAndApartmentSplits = addressSplits[1].replace(" ", "").split(APARTMENT_DELIMITER)
         mutableList.add(houseAndApartmentSplits[0])
         if (houseAndApartmentSplits.size > 1) {
             mutableList.add(houseAndApartmentSplits[1])
