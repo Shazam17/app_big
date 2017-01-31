acc8502281edd5e5192cfa1ecf083334e0a67781
Alexander Popov <chuffey.1240@ya.ru>
2017-01-31 11:03:49 +0300

    ER-84 Поправить замечания ч.3 - Проверка кода

--- app/Erkc/app/src/main/kotlin/com/software/ssp/erkc/modules/sendvalues/SendValuesActivity.kt
+++ app/Erkc/app/src/main/kotlin/com/software/ssp/erkc/modules/sendvalues/SendValuesActivity.kt
@@ -64,7 +64,7 @@ class SendValuesActivity : MvpActivity(), ISendValuesView {
         newIpus.forEach {
             val ipuLayout = layoutInflater.inflate(R.layout.sendparameters_ipu_layout, parametersContainer, false)
             ipuLayout.ipuLocation.text = it.installPlace
-            ipuLayout.ipuValueWrapper.hint = "${it.serviceName} (№${it.number})"
+            ipuLayout.ipuValueWrapper.hint = getString(R.string.send_values_ipu_hint).format(it.serviceName, it.number)
             ipuLayout.ipuValue.textChangedListener {
                 afterTextChanged {
                     text ->
